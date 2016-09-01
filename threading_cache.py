17ee4f5a93c9048fdf2399ce1020b0e1e409d3a3
jlzhu <jl_zhu@foxmail.com>
2016-09-01 20:06:08 +0800

    [REV] Update threading cache key

--- threading_cache.py
+++ threading_cache.py
@@ -1,21 +1,40 @@
 # -*- coding: utf-8 -*-
 
+import hashlib
 import threading
 from functools import wraps
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 local_context = threading.local()
+
+
+def _make_key(func, args, kwargs):
+    """
+    Serialize the param to hash value.
+    :param func:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    key = pickle.dumps((func.__name__, args, kwargs))
+    print key
+    return hashlib.md5(key).hexdigest()
 
 
 def local_cache(name=None):
     """
     Decorate function to cache result in local_context.
+    :param name: cache key name.
     """
     def _local_cache(func):
         @wraps(func)
         def _cache(*args, **kwargs):
             local_name = name
             if local_name is None:
-                local_name = 'func_' + func.__name__
+                local_name = _make_key(func, args, kwargs)
             data = getattr(local_context, local_name, None)
             if data is None:
                 data = func(*args, **kwargs)
@@ -28,7 +47,7 @@ def local_cache(name=None):
     return _local_cache
 
 
-@local_cache('data')
+@local_cache()
 def get_data():
     import random
     return random.randrange(1, 100)
