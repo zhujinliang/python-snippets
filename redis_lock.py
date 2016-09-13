5a3b8b4d19ee8e9c681034161e8309823af5e00e
jlzhu <jl_zhu@foxmail.com>
2016-09-13 11:36:49 +0800

    [REV] Update redis lock acquire

--- redis_lock.py
+++ redis_lock.py
@@ -9,13 +9,15 @@ __all__ = [
 ]
 
 
-def acquire_lock(lock_key, time=None):
+def acquire_lock(lock_key, time=600):
     '''
     Acquire lock.
     若lock不存在即当前状态为unlock，则获取到，返回True
     若lock存在即当前状态为locked，则未获取到，返回False
     '''
-    lock = redis.setnx(lock_key, 1, time)
+    lock = redis.setnx(lock_key, 1)
+    if lock:
+        redis.expire(lock_key, time)
     return lock
 
 
